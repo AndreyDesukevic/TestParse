4b59598f32faeacc274c1c53235fd7139a4b37e7
Андрей Десюкевич <70821681+AndreyDesukevic@users.noreply.github.com>
2022-08-10 21:31:35 +0300

    Add files via upload

---
+++ TestTaskParse/Rootobject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+public class Rootobject
+{
+    public Metadata metadata { get; set; }
+    public Data data { get; set; }
+}
+
+public class Metadata
+{
+    public string name { get; set; }
+    public string catalog_type { get; set; }
+    public string catalog_value { get; set; }
+}
+
+public class Data
+{
+    public List<Product> products { get; set; }
+}
+
+public class Product
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public string brand { get; set; }
+    public int feedbacks { get; set; }
+    public double priceU { get; set; }
+}
+
+
